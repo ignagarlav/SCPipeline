67d941561ca173a31a935f59d201b42143ca3805
Ignacio Garzon Alvarez <igarzonalva@hpc-2.unav.es>
2024-12-23 14:37:29 +0100

    Commit inicial

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     install_requires=[],                # Lista de dependencias externas si aplica
     description="Herramientas para el análisis de single cell RNA-Seq",  # Descripción del proyecto
     author="Jose Ignacio Garzón",                 # Autor del proyecto
-    author_email="igarzonalva@alumni.unav.es",  # Email de contacto
-    url="https://github.com/tu_usuario/mi_proyecto",  # URL del repositorio si aplica
+    author_email="igarzonalva@hpc-2.unav.es",  # Email de contacto
+    url="https://github.com/ignagarlalv/SCPipeline",  # URL del repositorio si aplica
 )
