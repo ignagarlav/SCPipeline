170ecab684fbec80a00fcd322bbe013e929371b4
Ignacio Garzon Alvarez <igarzonalva@hpc-2.unav.es>
2024-12-28 12:02:56 +0100

    Modified init to add a main and add export data to seurat function to utils

--- utils.py
+++ utils.py
@@ -37,3 +37,80 @@ def setup_logging(logger_name, log_file, level=logging.INFO):
 
 
     return logger
+
+
+
+def exportar_datosV1(adata, seurat_dir): 
+
+    # Importar los paquetes necesarios
+    import os 
+    import datetime
+    from anndata import AnnData
+    import pandas as pd
+    from scipy.io import mmwrite
+    import scipy
+    # Crear el directorio base si no existe
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    os.makedirs(seurat_dir, exist_ok=True)
+    fullPath = os.path.join(seurat_dir, current_date)
+    os.makedirs(fullPath, exist_ok=True)
+
+    # Exportar los datos de expresión normalizados y escalados (scaled data)
+    scaled = pd.DataFrame(
+        adata.X,  # Use raw data if available
+        index=adata.obs_names,  # Cell barcodes
+        columns=adata.var_names,  # Gene names
+    )
+    scaled_transposed = scaled.T
+    file_name = f"{current_date}_scaled.csv"
+    file_path = os.path.join(fullPath, file_name)
+    scaled_transposed.to_csv(file_path)
+
+       
+    # Save .var (gene metadata)
+    var_path = os.path.join(fullPath, "var.csv")
+    adata.var.to_csv(var_path)
+    print(f"Saved .var to {var_path}")
+
+
+    # Export metadata (e.g., cell type annotations, clustering)
+    file_name = f"{current_date}_metadata.csv"
+    file_path = os.path.join(fullPath, file_name)
+    adata.obs.to_csv(file_path)
+ 
+    # Save each layer in .layers
+    for layer_name, layer_data in adata.layers.items():
+        layer_path = os.path.join(fullPath, f"layer_{layer_name}_sparse.mtx")
+
+        if isinstance(adata.layers.get(layer_name), scipy.sparse.spmatrix):
+            mmwrite(layer_path, adata.layers[layer_name])
+        else:
+            layer_sparse = scipy.sparse.csr_matrix(adata.layers[layer_name])
+            mmwrite(layer_path, layer_sparse)
+
+        print(f"Saved {layer_name} in MTX format to {layer_path}")
+
+   
+    # Export UMAP coordinates
+    for key in adata.obsm.keys():
+            # Check if the key corresponds to a UMAP embedding
+            if key.startswith('X_umap'):
+                # Extract UMAP coordinates
+                umap_coords = pd.DataFrame(
+                    adata.obsm[key],
+                    index=adata.obs_names,
+                    columns=[f'{key}_1', f'{key}_2']
+                )
+                # Construct file name and path
+                file_name = f"{current_date}_{key}_coords.csv"
+                file_path = os.path.join(fullPath, file_name)
+            
+                # Save to CSV
+                umap_coords.to_csv(file_path)
+
+
+
+
+
+
+
