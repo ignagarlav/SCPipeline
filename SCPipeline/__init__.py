485a044a9de08d8e4c2d9e1d295fe7d5e24ee7fd
Jose Ignacio Garzón Álvarez <joseignaciogarzonalvarez@MacAir-de-Nacho.local>
2024-12-28 17:47:09 +0100

    Reestructuración del paquete SCPipeline y añadidos setup.py y __init__.py

---
+++ SCPipeline/__init__.py
@@ -0,0 +1,8 @@
+# SCPipeline/main.py
+
+def main():
+    print("Ejecutando el punto de entrada de SCPipeline")
+    # Llama a tu script o función principal aquí
+
+if __name__ == "__main__":
+    main()
